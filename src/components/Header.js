deef160216fff76bcdaad422274814b6dfe30ec1
mutai-gilbert <mutaigilbert85@gmail.com>
2023-05-27 20:43:21 +0300

    Add new files flex wrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <>
             <nav className='bg-green-600 py-4 text-white'>
-                <ul className='flex justify-center'>
+                <ul className='flex flex-wrap justify-center'>
                     <li className='mx-5 tracking-widest uppercase font-bold'><NavLink to="/" className='transition-colors hover:text-green-900'>Home</NavLink></li>
                     <li className='mx-5 tracking-widest uppercase font-bold'><NavLink to="/heels" className='transition-colors hover:text-green-900'>Heels</NavLink></li>
                     <li className='mx-5 tracking-widest uppercase font-bold'><NavLink to="/dresses" className='transition-colors hover:text-green-900'>Dresses</NavLink></li>
